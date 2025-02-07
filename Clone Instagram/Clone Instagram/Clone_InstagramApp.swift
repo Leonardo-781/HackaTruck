98ebb66a82d2268ae237238161aab606d2f9fcb5
Leonardo Cardoso <107780220+Leonardo-781@users.noreply.github.com>
2025-02-07 10:57:49 -0300

    Add files via upload

---
+++ Clone Instagram/Clone Instagram/Clone_InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clone_InstagramApp.swift
+//  Clone Instagram
+//
+//  Created by Turma01-17 on 07/02/25.
+//
+
+import SwiftUI
+
+@main
+struct Clone_InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
